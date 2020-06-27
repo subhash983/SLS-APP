39544c807b03d69c4eaee83beb9df102280bf762
Subhash Jha <subhash.983@gmail.com>
2020-06-27 19:10:13 +0530

    adding auth service

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -10,8 +10,17 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
+
+  if (email === auction.seller) {
+    throw new createError.Forbidden(`You cannot bid on your own auction`);
+  }
+
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder`);
+  }
 
   if (auction.status !== "OPEN") {
     throw new createError.Forbidden(`Auction is already closed`);
@@ -26,9 +35,11 @@ async function placeBid(event) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: "set highestBid.amount = :amount",
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder = :bidder",
     ExpressionAttributeValues: {
       ":amount": amount,
+      ":bidder": email,
     },
     ReturnValues: "ALL_NEW",
   };
